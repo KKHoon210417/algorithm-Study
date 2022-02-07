a714c5bbacd768b7e49ac42b1090d6782279c1ac
KKHoon210417 <zozo1591@naver.com>
2022-02-08 01:22:56 +0900

    추가 - 큰수의법칙 문제풀이_greedy

---
+++ BAEKJOON_JAVA/src/main/java/com/kwanghun/baekjoon_java/BaekjoonJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kwanghun.baekjoon_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekjoonJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaekjoonJavaApplication.class, args);
+    }
+
+}
